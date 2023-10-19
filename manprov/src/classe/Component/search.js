f5288421d3e0955d244942dd7e6038a94c56000e
I_Smail <106024751+ISmaillll@users.noreply.github.com>
2023-10-19 21:18:49 +0100

    Delete manprov/src directory

--- manprov/src/classe/Component/search.js
+++
@@ -1,9 +0,0 @@
-
-export function Search({search,changesearch,Search}){
-    return(<>
-            <div className='inputanim'>
-                <input className='w-50 text-light' onChange={changesearch} value={search} type="text" placeholder="Search..."/>
-                <button className="btn3" onClick={Search}><i class="fa fa-search" aria-hidden="true"></i></button>
-            </div>                   
-    </>)
-}
